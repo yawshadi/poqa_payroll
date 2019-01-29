dd08f4ed6dd7cd7084e7b72bebc6d390208c87f8
yaw shadrach <weway@yahoo.com>
2019-01-29 12:12:42 +0000

    leavereport

--- app/views/reports/leavereport.php
+++ app/views/reports/leavereport.php
@@ -15,6 +15,10 @@ tr, td{
   font-weight: 700;
   font-size: 12px;
 }
+.btn{
+  border-radius:0px !important;
+  margin-top:2px;
+}
 
 </style>
 
@@ -27,22 +31,17 @@ tr, td{
   <div class="container-fluid main_container" style='margin-top:-10px'>
 
       <div class="row">
-        <div class="col-12">
+        <div class="col-md-6">
           <h1 style='color:#FB6600; font-weight:700' class="page-title">Leave Report for <?= date('Y') ?></h1>
+
+        </div>
+        <div class="col-md-6">
+          <button onclick="window.print()"class='btn btn-primary pull-right'>Print</button>
+
         </div>
    </div>
 
       <hr/>
-
-      <div id='placeholder'>
-
-
-      <?php //require APPROOT .'/views/inc/dash.php' ; ?>
-
-
-      </div>
-
-
 
 <div class="row" style="margin-bottom:20px">
 
@@ -52,20 +51,19 @@ tr, td{
       <div class="container">
       <br/>
       <div>
-
       <table  class='table table-bordered table-condensed' style='font-size:12px'>
       <tr>
-          <td colspan="5" align='center' style='font-weight:700'>LEAVES REQUEST </td>
+          <td colspan="5" align='center' style='font-weight:700'>LEAVE DEDUCTIONS FOR <?= date('Y')?> </td>
         </tr>
       <?php
       $x=0; 
       foreach(Employee::listAll() as $employee):
       ?>
       <tr>
-      <td> <?= $employee->fullname ?></td>
+      <td style="padding-top:<?=($x==0)?'40px !important':'' ?>"> <?= $employee->fullname ?></td>
       <td>
       <table  style='font-size:12px; color:#000' class='table table-bordered'>
-        <tr style="display:<?=($x==0)?'':'none' ?>">
+        <tr style="font-weight:bold;display:<?=($x==0)?'':'none' ?>">
           <td>Leave entitled to </td>
           <td>Dates on Leave(From) </td>
           <td>Dates on Leave (To)</td>
@@ -75,19 +73,30 @@ tr, td{
         <?php
         $i=1;
         $leavedata = Leave::getLeave($employee->basic_id);
+        if(sizeof($leavedata) > 0):
          foreach($leavedata as $get):
         ?>
         <tr>
-        <td <?= ($i==1)?"rowspan=".sizeof($leavedata):"style=display:none"?>><?php echo $employee->accumulatedleave ?></td>
-          <td><?php echo $get->startdate  ?></td>
-          <td><?php echo $get->endate  ?></td>
-          <td><?php  echo $get->actualdays  ?></td>
-          <td <?= ($i==1)?"rowspan=".sizeof($leavedata):"style=display:none"?>><?php echo Leavedays::availabledays($employee->basic_id,date('Y')) ?></td>
+        <td width='15%' <?= ($i==1)?"rowspan=".sizeof($leavedata):"style=display:none"?>><?php echo $employee->accumulatedleave ?></td>
+          <td width='15%'><?php echo $get->startdate  ?></td>
+          <td width='15%'><?php echo $get->endate  ?></td>
+          <td width='15%'><?php  echo $get->actualdays  ?></td>
+          <td width='15%' <?= ($i==1)?"rowspan=".sizeof($leavedata):"style=display:none"?>><?php echo Leavedays::availabledays($employee->basic_id,date('Y')) ?></td>
         </tr>
         <?php
         $i++; 
         endforeach; 
+        else:
         ?>
+
+        <tr>
+         <td width='15%'><?php echo $employee->accumulatedleave ?></td>
+          <td width='15%'>-</td>
+          <td width='15%'>-</td>
+          <td width='15%'>-</td>
+          <td width='15%'><?php echo Leavedays::availabledays($employee->basic_id,date('Y')) ?></td>
+        </tr>
+        <?php endif; ?>
       </table>
       
       
