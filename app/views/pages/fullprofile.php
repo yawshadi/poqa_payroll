531b868f4aca4e575fb82a877a70d729ebaa7a85
yaw shadrach <weway@yahoo.com>
2019-02-19 09:48:15 +0000

    conflict resolved

--- app/views/pages/fullprofile.php
+++ app/views/pages/fullprofile.php
@@ -285,11 +285,7 @@ tr, td{
                 <a href='<?php echo URLROOT.'/payrollreport/payslip/'.$data['empdata']->basic_id  ?>' class='btn btn-danger'  style='font-size:12px; background:#BFFF00; color:#000'> Print Slip</a>
             </td>
             <td>
-<<<<<<< HEAD
             <a  class='btn btn-danger' id="employeefolder" style='font-size:12px;color:white'><i class="fa fa-folder-o"></i>Employee Folder</a>
-=======
-            <a  class='btn btn-danger' id="employeefolder" style='font-size:12px;color:white'><i class="fa fa-folder-o"></i> Employee Folder</a>
->>>>>>> workingshadi
 
           </td>
         </tr>
